80c4187eccd499d8acc3ec8a78d22b241268a659
manfrin92 <manfredd_10@hotmail.com>
2021-11-04 19:36:52 -0300

    feat: negative tweet

--- example2/map.py
+++ example2/map.py
@@ -9,6 +9,6 @@ for row in sys.stdin:
     CreationDate = fields[2];
     Tweet = fields[3];
 
-    if "Empresa1" in Tweet and "worst" not in Tweet and "bad" not in Tweet and "never" not in Tweet and "horrible" not in Tweet and "terrible" not in Tweet and "?" not in Tweet:
+    if ("Empresa1" in Tweet and "?" not in Tweet) and ("worst" in Tweet or "bad" in Tweet or "never" in Tweet or "horrible" in Tweet or "terrible" in Tweet):
         print '%s\t%s' % ("Empresa1", "1");
 		
